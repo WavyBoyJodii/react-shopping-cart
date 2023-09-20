0d5f76847716a69c1fc4ab7d06f43cdd35d6d828
WavyBoyJodii <najeecasey@gmail.com>
2023-09-20 15:23:19 -0400

    make ShopCard responsive

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -24,19 +24,21 @@ export default function ShopCard() {
     <Container>
       <div className="space-y-10 pb-10">
         <div className="p-4 sm:p-6 lg:p-8 rounded-lg overflow-hidden flex justify-center">
-          <Card className=" flex border-0  ">
+          <Card className=" flex  flex-col border-0  md:flex-row">
             <CardContent className="pt-4">
               <div className=" aspect-square relative bg-foreground/5 rounded-lg">
                 <img
                   src={item.image}
-                  className=" aspect-square object-cover rounded-lg transition-all duration-300 hover:scale-105"
+                  className=" aspect-square object-cover rounded-lg transition-all duration-300 hover:scale-105 m-auto"
                 />
               </div>
             </CardContent>
             <CardFooter className=" flex-col  justify-center items-center">
               <div className=" flex flex-col gap-6 bg-slate-100 items-center">
                 <p className=" font-semibold text-lg">{item.title}</p>
-                <p className=" text-base text-center">{item.description}</p>
+                <p className=" text-xs text-center md:text-base">
+                  {item.description}
+                </p>
                 <div className="flex justify-between pb-3 items-center gap-10">
                   <p className=" text-sm text-primary/80 space-y-3 ml-2">
                     {' '}
@@ -47,7 +49,12 @@ export default function ShopCard() {
                     action="cart"
                     className=" flex gap-2 mr-2"
                   >
-                    <input type="number" name="quantity" min="0" />
+                    <input
+                      type="number"
+                      name="quantity"
+                      min="0"
+                      className=" w-12"
+                    />
                     <Button size="sm" type="submit">
                       Add To Cart
                     </Button>
